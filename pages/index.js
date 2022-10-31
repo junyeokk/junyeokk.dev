f7dbaed428866264784d5f2f013a5005e454a639
junyeokk <cjh4302@gmail.com>
2022-10-31 10:00:30 +0900

    feat: tailwindcss 적용

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Layout from '../components/layout';
 
 export default function Home() {
@@ -12,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Main Page</h1>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     </Layout>
   )
 }
