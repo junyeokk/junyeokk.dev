09bf4173d0db27b2f172807dc1f9e9f058cbab66
junyeokk <cjh4302@gmail.com>
2023-02-06 22:12:54 +0900

    feat: simplify header

--- components/header.js
+++ components/header.js
@@ -7,18 +7,12 @@ export default function Header() {
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round"
-                             strokeLinejoin="round" strokeWidth="2"
-                             className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <Link className="ml-3 text-xl" href="/">junyeokk</Link>
+                        <Link className="ml-3 text-xl" href="/">junyeokk.dev</Link>
                     </div>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <Link className="mr-5 hover:text-gray-900" href="/">Home</Link>
-                        <Link className="mr-5 hover:text-gray-900" href="/about-me">About Me</Link>
-                        <Link className="mr-5 hover:text-gray-900" href="/projects">Projects</Link>
-                        <Link className="mr-5 hover:text-gray-900" href="/blog">Blog</Link>
+                        <Link className="mr-5 hover:text-gray-900" href="/about-me">about</Link>
+                        <Link className="mr-5 hover:text-gray-900" href="/projects">projects</Link>
+                        <Link className="mr-5 hover:text-gray-900" href="/posts">posts</Link>
                         <Link className="mr-5 hover:text-gray-900" href="https://open.kakao.com/o/saOFI3Ke">Contact</Link>
                     </nav>
                     <DarkModeToggleButton/>
