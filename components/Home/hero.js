080d61b08fa9fb52a71d236559830a4e61b3cf36
junyeokk <cjh4302@gmail.com>
2023-02-19 18:03:58 +0900

    refactor: hero 문구 수정

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
       </div>
       <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          안녕하세요! 😆 개발자를 꿈꾸는 대학생 채준혁입니다.
+          안녕하세요! 😆 프론트엔드 개발자를 지망하고 있는 대학생 채준혁입니다.
           <br className="hidden lg:inline-block" />
         </h1>
         <p className="mb-8 leading-relaxed">
