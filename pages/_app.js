06e15701dca65dd2d9c7b13feb8347063056aab2
junyeokk <cjh4302@gmail.com>
2023-02-22 18:40:30 +0900

    refactor: google analytics undefined

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import GoogleAnalytics from "../components/google-analytics/google-analytics";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <GoogleAnalytics />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
