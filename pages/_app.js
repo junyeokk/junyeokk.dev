108f3bf567e9e81b8dc14257c441d6b09d04a274
junyeokk <cjh4302@gmail.com>
2023-02-22 17:38:19 +0900

    feat: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
-import '../styles/globals.css'
-import { ThemeProvider } from 'next-themes'
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import GoogleAnalytics from "../components/google-analytics/google-analytics";
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <ThemeProvider attribute="class">
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
