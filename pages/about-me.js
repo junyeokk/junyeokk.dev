2087584b92370eaf09edbc773ac1135d0da7a181
junyeokk <cjh4302@gmail.com>
2023-02-23 18:16:54 +0900

    refactor: hide about page

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,9 +1,11 @@
 import Layout from "../components/layout";
 
 export default function AboutMe() {
-    return (
-        <Layout>
-            <h1>About me</h1>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="flex flex-col min-h-screen px-5 mb-10 px-6">
+        <div className="grid grid-cols-fill py-10 m-6 gap-8">준비중입니다.</div>
+      </div>
+    </Layout>
+  );
 }
