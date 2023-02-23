69783f2336cbcd51a9c76315ec98f4d8c9bb44e2
junyeokk <cjh4302@gmail.com>
2023-02-23 18:27:32 +0900

    refactor: post item

--- pages/posts.js
+++ pages/posts.js
@@ -6,7 +6,11 @@ export default function Posts({ posts }) {
   return (
     <Layout>
       <div className="flex flex-col min-h-screen px-5 mb-10 px-6">
-        <div className="grid grid-cols-fill py-10 m-6 gap-8">준비중입니다.</div>
+        <div className="grid grid-cols-fill py-10 m-6 gap-8">
+          {posts.results.map((aPost) => (
+            <PostItem key={aPost.id} data={aPost} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
