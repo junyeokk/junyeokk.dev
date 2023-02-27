b8af9c923d2cdb7924390fcb03f4559445084149
junyeokk <cjh4302@gmail.com>
2023-02-27 18:47:33 +0900

    feat: 최소 높이 수정

--- pages/about-me.js
+++ pages/about-me.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 export default function AboutMe() {
   return (
     <Layout>
-      <div className="flex flex-col min-h-screen px-5 mb-10 px-6">
+      <div className="flex flex-col min-height-calc px-5 mb-10 px-6">
         <div className="grid grid-cols-fill py-10 m-6 gap-8">준비중입니다.</div>
       </div>
     </Layout>
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className="flex min-h-screen flex-column items-center justify-center text-gray-600 body-font">
+      <section className="flex min-height-calc flex-column items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
