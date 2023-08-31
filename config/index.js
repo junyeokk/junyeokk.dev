df0ddf74ee6a87a64b0c510a25247862f3533a69
junyeokk <cjh4302@gmail.com>
2023-08-31 18:53:40 +0900

    feat: add features with travis' code

--- config/index.js
+++
@@ -1,3 +0,0 @@
-export const TOKEN = process.env.NOTION_TOKEN;
-export const DATABASE_PORTFOLIO_ID = process.env.NOTION_DATABASE_PORTFOLIO_ID;
-export const DATABASE_POSTS_ID = process.env.NOTION_DATABASE_POSTS_ID;
